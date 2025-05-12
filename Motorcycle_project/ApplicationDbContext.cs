2f8a86db0e5fc8b4bcdfcafe03e2b42d74d70f23
vladpogh <poghosyanvova22@gmail.com>
2025-05-12 10:31:38 +0300

    Add project files.

---
+++ Motorcycle_project/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Motorcycle> Motorcycles { get; set; }
+}
---
+++ Motorcycle_project/Models/Motorcycle.cs
@@ -0,0 +1,9 @@
+﻿public class Motorcycle
+{
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
