38d1c0682b440d8cf0cdea08227c24dbe2671d93
vladpogh <poghosyanvova22@gmail.com>
2025-05-13 08:35:17 +0300

    prog.cs cgange

--- Motorcycle_project/Program.cs
+++ Motorcycle_project/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
 
+// Register ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// The rest of the code...
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
