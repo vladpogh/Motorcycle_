23820475edcb3b464c564692458bf9ef16dbf2ce
vladpogh <poghosyanvova22@gmail.com>
2025-05-22 10:14:43 +0300

    controlles

---
+++ Motorcycle_project/Controllers/MotorcycleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotorcycleMarketplace.Models;
+
+public class MotorcycleController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public MotorcycleController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Motorcycles
+    public async Task<IActionResult> Index()
+    {
+        var motorcycles = await _context.Motorcycles.ToListAsync();
+        return View(motorcycles);
+    }
+
+    // GET: Motorcycles/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Motorcycles/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Brand,Model,Description,Price,ImageUrl")] Motorcycle motorcycle)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(motorcycle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(motorcycle);
+    }
+}
