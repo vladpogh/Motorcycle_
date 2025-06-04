20191c94fd15fdf4775175ddb01b55e05cf74b40
vladpogh <poghosyanvova22@gmail.com>
2025-06-04 13:33:56 +0300

    controller

--- Motorcycle_project/Models/Motorcycle.cs
+++ Motorcycle_project/Models/Motorcycle.cs
@@ -5,5 +5,5 @@
     public string Model { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public string ImageUrl { get; set; }
+    public string Image { get; set; }
 }
