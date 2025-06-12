c683c3f0a67dedaf176890d899e71e048ad81cdd
vladpogh <poghosyanvova22@gmail.com>
2025-06-12 09:22:55 +0300

    22

--- Motorcycle_project/Program.cs
+++ Motorcycle_project/Program.cs
@@ -16,5 +16,16 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
 
